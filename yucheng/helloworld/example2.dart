418d892cdffbb4c562acea59c38c9ec9b4c5adff
cycyucheng1010 <62127656+cycyucheng1010@users.noreply.github.com>
2021-07-28 18:24:49 +0800

    Add files via upload

---
+++ yucheng/helloworld/example2.dart
@@ -0,0 +1,21 @@
+import 'dart:io';
+import 'package:puppeteer/puppeteer.dart';
+
+void main() async {
+  // Start the browser and go to a web page
+  var browser = await puppeteer.launch();
+  var page = await browser.newPage();
+  await page.goto('https://dart.dev', wait: Until.networkIdle);
+
+  // For this example, we force the "screen" media-type because sometime
+  // CSS rules with "@media print" can change the look of the page.
+  await page.emulateMediaType(MediaType.screen);
+
+  // Capture the PDF and save it to a file.
+  await page.pdf(
+      format: PaperFormat.a4,
+      printBackground: true,
+      pageRanges: '1',
+      output: File('_dart.pdf').openWrite());
+  await browser.close();
+}
